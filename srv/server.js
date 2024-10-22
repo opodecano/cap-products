ad4e14c109424cc5663378c7a555e649e116d143
opodecano <opodecano@gmail.com>
2024-10-22 11:24:37 +0000

    commits

--- srv/server.js
+++ srv/server.js
@@ -1,14 +1,11 @@
-const createHandler = require('graphql-http');
 const cds = require("@sap/cds");
 const cors = require("cors");
 const adapterProxy = require("@cap-js-community/odata-v2-adapter");
-const schema = require("@graphql-tools/schema");
 
 
 cds.on("bootstrap", (app) => {
   app.use(adapterProxy());
   app.use(cors());
-  app.all('/graphql', createHandler({ schema }));
   app.get("/alive", (_, res) => {
     res.status(200).send("Server is Alive");
   });
